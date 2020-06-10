49ef3e20ebcc8f3ca6213ae0d8f26a0f9f2556f3
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-06-10 22:17:29 +0100

    adding a bundler

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,5 +1,5 @@
-var express = require("express");
-var path = require("path");
+import express from "express";
+const path = require("path");
 var open = require("open");
 
 var port = 3000;
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,2 @@
+import chalk from "chalk";
+console.log(chalk.green("Starting the App in Dev Mode..."));
