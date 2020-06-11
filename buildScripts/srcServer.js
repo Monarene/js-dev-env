c09d86a2b126c2645d70cbdd6a9f95c853456cb1
Michael Mekuleyi <mekuleyimichael@gmail.com>
2020-06-11 06:16:18 +0100

    added webpack config

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,19 @@
 import express from "express";
 const path = require("path");
 var open = require("open");
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 
 var port = 3000;
 var app = express();
+const compiler = webpack(config);
+
+app.use(
+	require("webpack-dev-middleware")(compiler, {
+		noInfo: true,
+		publicPath: config.output.publicPath,
+	})
+);
 
 app.get("/", function (req, res) {
 	res.sendFile(path.join(__dirname, "../src/index.html"));
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import numeral from "numeral";
+import "./index.css";
+
+const courseValue = numeral(1000).format("$0,0.00");
+debugger;
+console.log(`I would pay ${courseValue} for this awesome course`);
